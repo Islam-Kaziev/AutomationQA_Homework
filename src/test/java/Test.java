a9332c16b6d6f502599d0aa6993092070f815644
islam_kaziev <kaziev19691@gmail.com>
2022-11-11 18:07:14 +0300

    personal data removed

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,9 @@
-package src.test.java;
+
 
 public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("My name is Vasya");
+    }
 }
